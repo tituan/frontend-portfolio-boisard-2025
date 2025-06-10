4fe396bae38fc1e892a7e2246a512600f89cc214
antoine <antoine.boisard8@gmail.com>
2025-06-10 17:58:54 +0200

    add personnal projects

--- components/Project.js
+++ components/Project.js
@@ -26,14 +26,6 @@ const projects = [
     description:
       "Espace professionnel offrant une visibilité complète sur le catalogue de biens et les disponibilités de lots pour les partenaires.",
   },
-//   {
-//     title: "Altarea Patrimoine",
-//     img: "/img/altarea-patrimoine.png",
-//     alt: "Altarea Patrimoine",
-//     link: "https://www.altarea-patrimoine.com/",
-//     description:
-//       "Développement de la solution dédiée à la gestion de patrimoine immobilier pour les clients particuliers du groupe Altarea.",
-//   },
   {
     title: "Edouard Denis",
     img: "/img/edouard-denis.png",
@@ -44,21 +36,41 @@ const projects = [
   },
 ];
 
+const projectsPerso = [
+  {
+    title: "Bureau d'études Capsol",
+    img: "/img/capsol-portfolio.png",
+    alt: "Capsol",
+    link: "https://be-capsol.com/",
+    description:
+      "Conception complète du projet Capsol, de la création de l’identité visuelle au développement du site web responsive.",
+  },
+  {
+    title: "Kiddiz",
+    img: "/img/kiddiz-portfolio.png",
+    alt: "Kiddiz APP",
+    link: "#",
+    description:
+      "Kiddiz est une application dédiée à la revente de jouets et équipements pour enfants de 0 à 14 ans, entre particuliers.",
+  },
+];
+
+
 export default function Project() {
   return (
     <div id="project" className="project">
       <div className="project__container">
-        <p className="patchTxt">Projets</p>
+        
         
         <p className="project__description">
-        En tant que développeur web front-end au sein d'une agence de communication, j'ai contribué à la réalisation de projets variés allant des sites vitrines haut-de-gamme aux plateformes interactives. Mon rôle a principalement consisté à :
+    En tant que développeur web front-end, j’ai eu l’occasion de mener à bien une grande variété de projets, aussi bien dans un cadre professionnel que personnel. J’ai notamment travaillé sur des sites vitrines, des plateformes interactives ainsi que des applications mobiles.
         </p>
         <ul className="project__list">
             <li>Développer des interfaces modernes et réactives</li>
             <li>Optimiser l'expérience utilisateur grâce à des animations fluides et des parcours intuitifs</li>
             <li>Assurer la qualité technique (SEO, accessibilité, performances)</li>
         </ul>
-
+        <p className="patchTxt">Projets Agence</p>
         <div className="project__wrapper">
           {projects.map((proj, i) => (
             <div className="item" key={proj.title}>
@@ -95,6 +107,48 @@ export default function Project() {
               </div>
             </div>
           ))}
+          
+          
+        </div>
+        <p className="patchTxt">Projets personnels</p>
+        <div className="project__wrapper">
+          {projectsPerso.map((proj, i) => (
+            <div className="item" key={proj.title}>
+              <div className="item__top">
+                <Image
+                  src={proj.img}
+                  alt={proj.alt}
+                  width={500}
+                  height={500}
+                  className="item__top__pic__img"
+                />
+                {proj.isModal && (
+                  <div className="item__top__action">
+                    <div
+                      className="item__top__action__btn js-video-play js-modal-open"
+                      data-modal={`video-${i}`}
+                    >
+                      {/* icon */}
+                    </div>
+                  </div>
+                )}
+              </div>
+              <div className="item__content">
+                <p className="item__content__title">{proj.title}</p>
+                <p className="item__content__description">{proj.description}</p>
+                <a
+                  href={proj.link}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="item__content__link"
+                >
+                  Accéder au site
+                </a>
+              </div>
+            </div>
+          ))}
+          
+          
         </div>
       </div>
     </div>
