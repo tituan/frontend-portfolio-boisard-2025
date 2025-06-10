c21142ff15e47085563d37c862e9961a51a852ca
antoine <antoine.boisard8@gmail.com>
2025-06-10 19:48:39 +0200

    chnage presentation text

--- components/Presentation.js
+++ components/Presentation.js
@@ -10,14 +10,14 @@ export default function Presentation() {
             <p className="patchTxt">présentation</p>
             <h3 className="presentation__content__title">Créer des expériences web innovantes</h3>
             <p className="presentation__content__text">
-              Je suis heureux de me présenter en tant que développeur web front-end, fort de cinq années d&rsquo;expérience dans un domaine aussi stimulant qu&rsquo;en perpétuelle évolution. Cette période m&rsquo;a permis de contribuer à une grande variété de projets, de consolider mes compétences techniques et de cultiver une véritable passion pour la création d&rsquo;interfaces à la fois esthétiques et performantes.
+              Développeur web front-end depuis cinq ans, j’ai contribué à des projets variés mêlant esthétique, performance et accessibilité. Passionné par la création d’interfaces intuitives, je veille à offrir une expérience utilisateur optimale. Curieux et rigoureux, je me tiens à jour des évolutions du web, et j’apprécie particulièrement le travail en équipe, convaincu que la diversité des idées mène à des solutions de qualité.
             </p>
-            <p className="presentation__content__text">
+            {/* <p className="presentation__content__text">
               Au fil des années, j&rsquo;ai développé des compétences solides en matière de création d&rsquo;interfaces utilisateur réactives et intuitives. Je suis particulièrement attentif aux détails et à l&rsquo;accessibilité, en m&rsquo;assurant que chaque utilisateur, quels que soient ses besoins, puisse profiter pleinement de l&rsquo;expérience en ligne.
             </p>
             <p className="presentation__content__text">
               Au-delà des compétences techniques, je suis un fervent défenseur de l&rsquo;apprentissage continu. Je suis toujours avide de me tenir au courant des dernières innovations dans le domaine du développement web et d&rsquo;améliorer mes compétences. J&rsquo;aime également travailler en équipe et collaborer avec des professionnels de différents horizons, car je crois fermement que l&rsquo;échange d&rsquo;idées et la diversité des perspectives peuvent conduire à des solutions exceptionnelles.
-            </p>
+            </p> */}
           </div>
           {/* <Link href="#" className="presentation__content__link">Mon CV</Link> */}
         </div>
