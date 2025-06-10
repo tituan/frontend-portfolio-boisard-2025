69a72a0e2f991220dc7911e1f14bfb69865405a0
antoine <antoine.boisard8@gmail.com>
2025-06-10 13:46:05 +0200

    add slick slider

--- components/Interest.js
+++ components/Interest.js
@@ -1,52 +1,99 @@
+'use client';
 import Link from 'next/link';
 import Image from "next/image";
+import Slider from "react-slick";
+
+// Liste des images de la galerie
+const galleryItems = [
+  {
+    src: "/img/bronx_slide.jpg",
+    alt: "Bronx Slide",
+    link: "https://picsum.photos/640/360",
+  },
+  {
+    src: "/img/death.jpg",
+    alt: "Death",
+    link: "https://picsum.photos/640/360?2",
+  },
+  {
+    src: "/img/bronx2_slide.jpg",
+    alt: "Bronx 2 Slide",
+    link: "https://picsum.photos/640/360?3",
+  },
+  {
+    src: "/img/player2.jpg",
+    alt: "Player 2",
+    link: "https://picsum.photos/640/360?4",
+  },
+  {
+    src: "/img/bronx3_slide.jpg",
+    alt: "Bronx 3 Slide",
+    link: "https://picsum.photos/640/360?5",
+  },
+];
+
+const interests = [
+  "#PHOTOGRAPHIE",
+  "#MUSIQUE",
+  "#ART",
+  "#BASKET",
+];
 
 export default function Interest() {
+  // Réglages du slider Slick
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2, // ou 1 en mobile
+    slidesToScroll: 1,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 768, // mobile
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <div id="interest" className="interest">
-        <div className="interest__container">
-            <p className="patchTxt">Intérêts</p>
-            <div className="interest__content">
-                <p className="interest__content__title">Developpeur web mais pas que !</p>
-                <p className="interest__content__text">La photographie est une passion qui me permet d'exprimer ma créativité et de capturer des moments précieux.  À travers mes clichés, j'essaie de transmettre des émotions et de raconter des histoires visuelles.</p>
-                <p className="interest__content__text">La musique et l'art occupent une place essentielle dans ma vie. Ils me permettent d'explorer ma créativité, de m'exprimer et d'apprécier la beauté qui nous entoure.</p>
-            </div>
-            <div className="interest__galery">
-                <div className="interest__galery__wrapper">
-                    <div href="https://picsum.photos/640/360"  className="itemGalery">
-                        <picture className="itemGalery__pic">	
-                            {/* <img src="dist/img/bronx_slide.jpg" alt="" className="itemGalery__pic__img"> */}
-                        </picture>
-                    </div>
-                    <div className="itemGalery">
-                        <picture className="itemGalery__pic">	
-                            {/* <img src="dist/img/death.jpg" alt="" className="itemGalery__pic__img"> */}
-                        </picture>
-                    </div>
-                    <div className="itemGalery">
-                        <picture className="itemGalery__pic">	
-                            {/* <img src="dist/img/bronx2_slide.jpg" alt="" className="itemGalery__pic__img"> */}
-                        </picture>
-                    </div>
-                    <div className="itemGalery">
-                        <picture className="itemGalery__pic">	
-                            {/* <img src="dist/img/player2.jpg" alt="" className="itemGalery__pic__img"> */}
-                        </picture>
-                    </div>
-                    <div className="itemGalery">
-                        <picture className="itemGalery__pic">	
-                            {/* <img src="dist/img/bronx3_slide.jpg" alt="" className="itemGalery__pic__img"> */}
-                        </picture>
-                    </div>
-                </div>
-            </div>
-            <div className="interest__more">
-                <p className="">#BASKET</p>
-                <p className="">#BASKET</p>
-                <p className="">#BASKET</p>
-                <p className="">#BASKET</p>
-            </div>
+      <div className="interest__container">
+        <p className="patchTxt">Intérêts</p>
+        <div className="interest__content">
+          <p className="interest__content__title">Développeur web mais pas que !</p>
+          <p className="interest__content__text">
+            La photographie est une passion qui me permet d'exprimer ma créativité et de capturer des moments précieux. À travers mes clichés, j'essaie de transmettre des émotions et de raconter des histoires visuelles.
+          </p>
+          <p className="interest__content__text">
+            La musique et l'art occupent une place essentielle dans ma vie. Ils me permettent d'explorer ma créativité, de m'exprimer et d'apprécier la beauté qui nous entoure.
+          </p>
         </div>
+        <div className="interest__galery">
+          <Slider {...settings} className="interest__galery__wrapper">
+            {galleryItems.map((item, i) => (
+              <div className="itemGalery" key={i}>
+                <Link href={item.link} target="_blank" rel="noopener noreferrer" className="itemGalery__pic">
+                  <Image
+                    src={item.src}
+                    alt={item.alt}
+                    width={400}
+                    height={250}
+                    className="itemGalery__pic__img"
+                  />
+                </Link>
+              </div>
+            ))}
+          </Slider>
+        </div>
+        <div className="interest__more">
+          {interests.map((interest, i) => (
+            <p key={i} className="">{interest}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import '../../styles/pages/home.scss';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
 import Presentation from "../../components/Presentation";
